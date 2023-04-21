0d84fee02ead0e5c055f0a66bd618f1676d92b8b
Fabricio <fepafumi97@gmail.com>
2023-04-21 17:44:08 -0300

    style: :lipstick: Added green colors palette

--- app/src/main/java/com/example/tenisapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/tenisapp/ui/theme/Color.kt
@@ -2,9 +2,12 @@ package com.example.tenisapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Green = Color(0xFF8FC93A)
+val Jade = Color(0xFF3BB273)
+val MidnightGreen = Color(0xFF044750)
+
+val Licorice = Color(0xFF261C15)
+val BabyPowder = Color(0xFFF7F7F2)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
