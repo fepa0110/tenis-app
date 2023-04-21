bc438629e03b6a5c2420504ee911b753d3644823
Fabricio <fepafumi97@gmail.com>
2023-04-21 17:49:15 -0300

    style: :lipstick: Added SmallLabel typo

--- app/src/main/java/com/example/tenisapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/tenisapp/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.example.tenisapp.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -14,6 +15,14 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp,
+        color = Color.White
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
@@ -23,12 +32,5 @@ val Typography = Typography(
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
     */
 )
