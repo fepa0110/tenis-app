4cce96180711a8b42d7eefac3f49eed30eaba96a
Fabricio <fepafumi97@gmail.com>
2023-04-24 14:22:40 -0300

    style: :construction: Styling welcome screen

--- app/src/main/java/com/example/tenisapp/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/tenisapp/screens/WelcomeScreen.kt
@@ -3,15 +3,9 @@ package com.example.tenisapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -28,7 +22,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -45,22 +45,39 @@ fun WelcomeScreen(
 ) {
     Surface(
         modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.tertiary
+        color = MaterialTheme.colorScheme.background
     ) {
+
         Column(
-            verticalArrangement = Arrangement.SpaceAround,
+            //verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(
-                text = "Bienvenido!",
-                fontSize = 32.sp,
-                modifier = modifier
-            )
-            Row() {
-                Button(onClick = onNavigateToTournaments) {
-                    Text(
-                        text = "Comenzar",
-                        fontSize = 16.sp,
-                    )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Bienvenido!",
+                    color = MaterialTheme.colorScheme.secondary,
+                    fontSize = 32.sp,
+                    modifier = Modifier
+                        .aspectRatio(16f / 7f)
+                        .paint(
+                            painter = painterResource(R.drawable.welcome),
+                            contentScale = ContentScale.FillWidth
+                        ),
+                )
+            }
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                Row() {
+                    Button(onClick = onNavigateToTournaments) {
+                        Text(
+                            text = "Comenzar",
+                            fontSize = 16.sp,
+                        )
+                    }
                 }
             }
         }
