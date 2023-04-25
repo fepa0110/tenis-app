fe6ceb9682d39e2b99fc9801478db63369068086
Fabricio <fepafumi97@gmail.com>
2023-04-25 18:07:32 -0300

    feat: :sparkles: Added TobBar

--- app/src/main/java/com/example/tenisapp/screens/TournamentsScreen.kt
+++ app/src/main/java/com/example/tenisapp/screens/TournamentsScreen.kt
@@ -13,11 +13,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,6 +24,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
@@ -32,20 +32,52 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.tenisapp.ui.theme.TenisAppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TournamentsScreen(
-    modifier: Modifier = Modifier,
+    modifier: Modifier,
     tournaments: List<String> = listOf("Grand Slam", "ATP Tour")
 ) {
-
-    LazyColumn {
-        for (tournament in tournaments) {
-            item {
-                TournamentRow(tournament)
-                Spacer(Modifier.size(12.dp))
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "Torneos",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Favorite,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                }
+            )
+        },
+        content = { innerPadding ->
+            LazyColumn(contentPadding = innerPadding, modifier = Modifier.padding(vertical = 5.dp)) {
+                for (tournament in tournaments) {
+                    item {
+                        TournamentRow(tournament)
+                        Spacer(Modifier.size(12.dp))
+                    }
+                }
             }
         }
-    }
+    )
+
 }
 
 @Composable
